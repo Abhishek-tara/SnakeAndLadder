149e315349a74775b4be5bec64c51ea78821c2a1
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-04-25 08:19:51 +0530

    Added: Player one

---
+++ Snake_LadderProblem/Snake_LadderProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Snake_LadderProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Welcome to the Snake and Ladder Game");
+            SnakeLadder newGame= new SnakeLadder();
+            newGame.GameMethod();
+        }
+    }
+}
---
+++ Snake_LadderProblem/Snake_LadderProblem/SnakeLadder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_LadderProblem
+{
+    public class SnakeLadder
+    {
+        public void GameMethod()
+        {
+            int position = 0;
+            Console.WriteLine("Welcome Player 1, Let's start the Game");
+            Console.WriteLine("Player 1 is at position : " + position);
+        }
+    }
+}
